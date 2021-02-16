cb1eb3719bc80dd5204924ed7174b7178a661b74
dahn510 <dahn510@gmail.com>
2021-02-16 22:47:51 +0000

    added on_message function

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,36 @@
 import os
+
 import discord
 from dotenv import load_dotenv
 
-#Get Discord token stored in .env file
+#Get token from .env
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.client()
+client = discord.Client()
 
+@client.event
+async def on_message(message):
+    #Ignore message sent by this bot
+    if message.author==client.user:
+        return;
+
+    print(f'{message.author}:', message.clean_content);
+
+@client.event
+async def on_message_delete(message):
+    #Ignore message sent by this bot
+    if message.author==client.user:
+        return;
+    print(f'(Deleted) {message.author}:', message.clean_content);
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} is connected to:')
 
+    #Print all the connected guilds
+    for guild in client.guilds:
+        print(f'{guild.name} (id: {guild.id})')    
 
 
 
